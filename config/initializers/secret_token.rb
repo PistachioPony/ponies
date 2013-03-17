d871ca037010649e9c5a65b51dd81f29284134d3
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-03-17 16:59:00 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ponies::Application.config.secret_token = 'bfec22114ea58d75b707bb5e1336b952da22ce6e3ad403faa809822fbd37569e434302da1ebea626341ddac1c14a2e8c1ae1674a5d479a875f3bc84007c88c1c'
